83cfecc2aeaf4cbc171200c6c7c4868cc18f0064
M Monster <horatius83@gmail.com>
2021-05-12 17:28:50 -0500

    Added values and deal functions

--- src/Deck.elm
+++ src/Deck.elm
@@ -1,16 +1,32 @@
-module Deck exposing (..)
+module Deck exposing (Deck, deal, values)
 
 import Array
 import Random   
+import Set
 import Card
 
 type alias Deck = List Card.Card
 
-deal : Deck -> Maybe (Deck, Card.Card)
-deal deck =
-    case deck of
-        (card::rest) -> Just (rest, card)
+deal : Deck -> Deck -> Maybe (Deck, Deck, Card.Card)
+deal deck discard =
+    case (deck, discard) of
+        ((card::rest), _) -> Just (rest, discard, card)
+        (_, (card::rest)) -> Just (rest, deck, card) 
         _ -> Nothing
 
-
-
+values : Deck -> Set.Set Int
+values deck = 
+    case deck of
+        (card::rest) ->
+            let
+                otherValues = values rest
+                thisValues = Set.fromList <| Card.values card
+                allCardValues = Set.foldl 
+                    (\thisValue a -> Set.foldl 
+                        (\otherValue s -> Set.insert (otherValue + thisValue) s) 
+                        Set.empty 
+                        otherValues
+                    ) Set.empty thisValues
+            in
+            allCardValues
+        _ -> Set.empty
