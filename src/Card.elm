3b9d73c280548c5ae99eb4038fbd1f970e79da95
M Monster <horatius83@gmail.com>
2021-10-09 15:24:16 -0500

    formatted Main.elm and Card.elm

--- src/Card.elm
+++ src/Card.elm
@@ -1,79 +1,177 @@
-module Card exposing (
-    Rank(..),
-    Suit(..),
-    Card,
-    values, 
-    getAllRanks, 
-    getAllSuits, 
-    getCardFrontHex, 
-    cardBackHex, 
-    getColor) 
+module Card exposing
+    ( Card
+    , Rank(..)
+    , Suit(..)
+    , cardBackHex
+    , getAllRanks
+    , getAllSuits
+    , getCardFrontHex
+    , getColor
+    , values
+    )
 
+import Html exposing (Html)
 import List
 import String.UTF32 as UTF32
 
-type Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
-type Suit = Clubs | Diamonds | Hearts | Spades
-type alias Card = {rank: Rank, suit: Suit}
+
+type Rank
+    = Two
+    | Three
+    | Four
+    | Five
+    | Six
+    | Seven
+    | Eight
+    | Nine
+    | Ten
+    | Jack
+    | Queen
+    | King
+    | Ace
+
+
+type Suit
+    = Clubs
+    | Diamonds
+    | Hearts
+    | Spades
+
+
+type alias Card =
+    { rank : Rank, suit : Suit }
+
 
 values : Card -> List Int
-values {rank, suit } =
+values { rank, suit } =
     case rank of
-        Two -> [2]
-        Three -> [3]
-        Four -> [4]
-        Five -> [5]
-        Six -> [6]
-        Seven -> [7]
-        Eight -> [8]
-        Nine -> [9]
-        Ten -> [10]
-        Jack -> [10]
-        Queen -> [10]
-        King -> [10]
-        Ace -> [1, 11]
+        Two ->
+            [ 2 ]
+
+        Three ->
+            [ 3 ]
+
+        Four ->
+            [ 4 ]
+
+        Five ->
+            [ 5 ]
+
+        Six ->
+            [ 6 ]
+
+        Seven ->
+            [ 7 ]
+
+        Eight ->
+            [ 8 ]
+
+        Nine ->
+            [ 9 ]
+
+        Ten ->
+            [ 10 ]
+
+        Jack ->
+            [ 10 ]
+
+        Queen ->
+            [ 10 ]
+
+        King ->
+            [ 10 ]
+
+        Ace ->
+            [ 1, 11 ]
+
 
 getAllRanks : List Rank
-getAllRanks = 
-        [Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace]
+getAllRanks =
+    [ Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace ]
+
 
 getAllSuits : List Suit
-getAllSuits = [Clubs, Diamonds, Hearts, Spades]
+getAllSuits =
+    [ Clubs, Diamonds, Hearts, Spades ]
+
 
 getCardFrontHex : Card -> String
-getCardFrontHex {rank, suit} =
+getCardFrontHex { rank, suit } =
     let
         rankHex =
             case rank of
-                Ace -> 0x1
-                Two -> 0x2
-                Three -> 0x3
-                Four -> 0x4
-                Five -> 0x5
-                Six -> 0x6
-                Seven -> 0x7
-                Eight -> 0x8
-                Nine -> 0x9
-                Ten -> 0xa
-                Jack -> 0xb
-                Queen -> 0xd
-                King -> 0xe
+                Ace ->
+                    0x01
+
+                Two ->
+                    0x02
+
+                Three ->
+                    0x03
+
+                Four ->
+                    0x04
+
+                Five ->
+                    0x05
+
+                Six ->
+                    0x06
+
+                Seven ->
+                    0x07
+
+                Eight ->
+                    0x08
+
+                Nine ->
+                    0x09
+
+                Ten ->
+                    0x0A
+
+                Jack ->
+                    0x0B
+
+                Queen ->
+                    0x0D
+
+                King ->
+                    0x0E
+
         suitHex =
             case suit of
-                Spades -> 0xa0
-                Hearts -> 0xb0
-                Diamonds -> 0xc0
-                Clubs -> 0xd0
+                Spades ->
+                    0xA0
+
+                Hearts ->
+                    0xB0
+
+                Diamonds ->
+                    0xC0
+
+                Clubs ->
+                    0xD0
     in
-        UTF32.toString [0x1f000 + suitHex + rankHex]
+    UTF32.toString [ 0x0001F000 + suitHex + rankHex ]
+
 
 cardBackHex : String
-cardBackHex = UTF32.toString [0x1f0a0]
+cardBackHex =
+    UTF32.toString [ 0x0001F0A0 ]
+
 
 getColor : Suit -> String
 getColor suit =
-    case suit of 
-        Spades -> "black"
-        Hearts -> "red"
-        Diamonds -> "red"
-        Clubs -> "black"
+    case suit of
+        Spades ->
+            "black"
+
+        Hearts ->
+            "red"
+
+        Diamonds ->
+            "red"
+
+        Clubs ->
+            "black"
--- src/Main.elm
+++ src/Main.elm
@@ -1,52 +1,70 @@
 module Main exposing (main)
-import Card exposing (Rank(..), Suit(..), Card, getCardFrontHex, cardBackHex, getColor)
-import String.UTF32 as UTF32
 
 import Browser
+import Card exposing (Card, Rank(..), Suit(..), cardBackHex, getCardFrontHex, getColor)
 import Html exposing (..)
-import Html.Events exposing (onClick)
 import Html.Attributes exposing (attribute)
+import Html.Events exposing (onClick)
+import List
 import Random
 import Random.List
-import List
+import String.UTF32 as UTF32
 
-type alias Model = List Card
 
-init : () -> (Model, Cmd Msg)
-init _ = ([(Card Ace Spades), (Card Queen Hearts)], Cmd.none)
+type alias Model =
+    List Card
+
+
+type Msg
+    = ShuffleNumbers
+    | NewList Model
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( [ Card Ace Spades, Card Queen Hearts ], Cmd.none )
+
 
 displayCard : Card -> Html Msg
-displayCard {rank, suit} = 
-    let 
-        color = getColor suit
-        cardClass = "card " ++ color
-        card = Card rank suit
+displayCard { rank, suit } =
+    let
+        color =
+            getColor suit
+
+        cardClass =
+            "card " ++ color
+
+        card =
+            Card rank suit
     in
-        span [attribute "class" cardClass] [text (getCardFrontHex card)]
+    span [ attribute "class" cardClass ] [ text (getCardFrontHex card) ]
+
 
 view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick ShuffleNumbers ]
-            [text "Shuffle List" ]
+            [ text "Shuffle List" ]
         , ul [] (List.map displayCard model)
-        , div [] [text cardBackHex]
+        , div [] [ text cardBackHex ]
         ]
 
-type Msg = ShuffleNumbers | NewList Model
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ShuffleNumbers -> (model, Random.generate NewList (Random.List.shuffle model))
-        NewList numbers -> (numbers, Cmd.none)
+        ShuffleNumbers ->
+            ( model, Random.generate NewList (Random.List.shuffle model) )
+
+        NewList numbers ->
+            ( numbers, Cmd.none )
+
 
 main : Program () Model Msg
 main =
     Browser.element
-    {
-        init = init,
-        view = view,
-        update = update,
-        subscriptions = \_ -> Sub.none
-    }
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
