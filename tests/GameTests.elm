fd89601da62a3b6a325910fa91b5a505b318d247
horatius83 <horatius83@gmail.com>
2022-02-13 14:53:23 -0600

    Added tests for double-down, insurance, and surrender

--- tests/GameTests.elm
+++ tests/GameTests.elm
@@ -74,5 +74,83 @@ gameTests =
                     in
                     Game.getBetResult dealerHand playerHandWithDoubleDown
                         |> Expect.equal (bet * 2)
+            , test "Lose, Double-Down: Dealer has higher hand than Player" <|
+                \_ -> 
+                    let
+                        bet = 100
+                        playerHand = Hand.create [Card Ten Clubs, Card Eight Hearts] bet
+                        playerHandWithDoubleDown = { playerHand | doubleDown = True, bet = bet * 2}
+                        dealerHand = [Card Ten Spades, Card Queen Hearts]
+                    in
+                    Game.getBetResult dealerHand playerHandWithDoubleDown
+                        |> Expect.equal (-bet * 2)
+            , test "Surrender: Dealer has higher hand than Player" <|
+                \_ -> 
+                    let
+                        bet = 100
+                        halfBet = (\x -> x * -1) <| Game.getHalfBet bet
+                        playerHand = Hand.create [Card Ten Clubs, Card Eight Hearts] bet
+                        playerWithSurrender = { playerHand | surrendered = True }
+                        dealerHand = [Card Ten Spades, Card Queen Hearts]
+                    in
+                    Game.getBetResult dealerHand playerWithSurrender
+                        |> Expect.equal halfBet
+            , test "Surrender: Dealer has lower hand than Player" <|
+                \_ -> 
+                    let
+                        bet = 100
+                        halfBet = (\x -> x * -1) <| Game.getHalfBet bet
+                        playerHand = Hand.create [Card Ten Clubs, Card Ace Hearts] bet
+                        playerWithSurrender = { playerHand | surrendered = True }
+                        dealerHand = [Card Ten Spades, Card Queen Hearts]
+                    in
+                    Game.getBetResult dealerHand playerWithSurrender
+                        |> Expect.equal halfBet
+            , test "Insurance: Dealer has a blackjack and player does not" <|
+                \_ -> 
+                    let
+                        bet = 100
+                        halfBet = Game.getHalfBet bet
+                        expectedBet = bet - (2 * halfBet)
+                        playerHand = Hand.create [Card Ten Clubs, Card Eight Hearts] bet
+                        playerWithInsurance = { playerHand | insurance = True }
+                        dealerHand = [Card Ten Spades, Card Ace Hearts]
+                    in
+                    Game.getBetResult dealerHand playerWithInsurance
+                        |> Expect.equal expectedBet
+            , test "Insurance: Dealer has a blackjack and so does the player" <|
+                \_ -> 
+                    let
+                        bet = 100
+                        playerHand = Hand.create [Card Ten Clubs, Card Ace Hearts] bet
+                        playerWithInsurance = { playerHand | insurance = True }
+                        dealerHand = [Card Ten Spades, Card Ace Hearts]
+                    in
+                    Game.getBetResult dealerHand playerWithInsurance
+                        |> Expect.equal bet
+            , test "Insurance: Dealer does not have a blackjack but has a better hand than Player" <|
+                \_ -> 
+                    let
+                        bet = 100
+                        halfBet = (\x -> x * -1) <| Game.getHalfBet bet
+                        expectedBet = -bet + halfBet
+                        playerHand = Hand.create [Card Ten Clubs, Card Eight Hearts] bet
+                        playerWithInsurance = { playerHand | insurance = True }
+                        dealerHand = [Card Ten Spades, Card Queen Hearts]
+                    in
+                    Game.getBetResult dealerHand playerWithInsurance
+                        |> Expect.equal expectedBet
+            , test "Insurance: Dealer does not have a blackjack and has a worse hand than Player" <|
+                \_ -> 
+                    let
+                        bet = 100
+                        halfBet = (\x -> x * -1) <| Game.getHalfBet bet
+                        expectedBet = bet + halfBet
+                        playerHand = Hand.create [Card Ten Clubs, Card Ace Hearts] bet
+                        playerWithInsurance = { playerHand | insurance = True }
+                        dealerHand = [Card Ten Spades, Card Queen Hearts]
+                    in
+                    Game.getBetResult dealerHand playerWithInsurance
+                        |> Expect.equal expectedBet
             ]
         ]
