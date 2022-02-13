857195921e63ef6f82cd38a153873d6b9baf7b17
horatius83 <horatius83@gmail.com>
2022-02-13 14:35:29 -0600

    Added more unit tests

--- tests/GameTests.elm
+++ tests/GameTests.elm
@@ -37,5 +37,42 @@ gameTests =
                     in
                     Game.getBetResult dealerHand playerHand
                         |> Expect.equal bet
+            , test "Win: Dealer busts" <|
+                \_ -> 
+                    let
+                        bet = 100
+                        playerHand = Hand.create [Card Ace Spades, Card Queen Hearts] bet
+                        dealerHand = [Card Eight Clubs, Card Eight Hearts, Card Eight Diamonds]
+                    in
+                    Game.getBetResult dealerHand playerHand
+                        |> Expect.equal bet
+            , test "Lose: Player busts" <|
+                \_ -> 
+                    let
+                        bet = 100
+                        playerHand = Hand.create [Card Eight Clubs, Card Eight Hearts, Card Eight Diamonds] bet
+                        dealerHand = [Card Ace Spades, Card Queen Hearts]
+                    in
+                    Game.getBetResult dealerHand playerHand
+                        |> Expect.equal (-bet)
+            , test "Lose: Both Dealer and Player busts" <|
+                \_ -> 
+                    let
+                        bet = 100
+                        playerHand = Hand.create [Card Eight Clubs, Card Eight Hearts, Card Eight Diamonds] bet
+                        dealerHand = [Card Ace Spades, Card Queen Hearts, Card King Clubs, Card Three Spades]
+                    in
+                    Game.getBetResult dealerHand playerHand
+                        |> Expect.equal (-bet)
+            , test "Win, Double-Down: Dealer has lower hand than Player" <|
+                \_ -> 
+                    let
+                        bet = 100
+                        playerHand = Hand.create [Card Ten Clubs, Card Eight Hearts] bet
+                        playerHandWithDoubleDown = { playerHand | doubleDown = True, bet = bet * 2}
+                        dealerHand = [Card Seven Spades, Card Queen Hearts]
+                    in
+                    Game.getBetResult dealerHand playerHandWithDoubleDown
+                        |> Expect.equal (bet * 2)
             ]
         ]
